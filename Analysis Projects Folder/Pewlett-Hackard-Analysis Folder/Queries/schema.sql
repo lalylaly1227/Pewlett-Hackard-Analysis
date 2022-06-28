2a2c877d580d0bd53256ed0ae625c6c8c259ea13
Gladys Robles <105124485+lalylaly1227@users.noreply.github.com>
2022-06-28 16:51:04 -0400

    updating files

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
@@ -1,27 +1,3 @@
- SELECT * FROM retirement_info;
- 
- SELECT first_name, last_name 
- INTO retirement_info 
- FROM employees 
- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
- -- Number of employees retiring SELECT COUNT(first_name) FROM employees WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
- -- Retirement eligibility SELECT first_name, last_name FROM employees WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
- -- Retirement eligibility SELECT first_name, last_name FROM employees WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
- 
- SELECT first_name, last_name FROM employees 
- WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
- 
- SELECT first_name, last_name FROM employees 
- WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
- 
- SELECT first_name, last_name FROM employees 
- WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
- 
- select * from departments;
- 
- select * from titles;
- 
  CREATE TABLE titles ( emp_no INT NOT NULL, title VARCHAR(50) NOT NULL, from_date DATE NOT NULL, to_date DATE, FOREIGN KEY (emp_no) REFERENCES employees (emp_no), PRIMARY KEY (emp_no, title, from_date) );
  
  drop table titles;
